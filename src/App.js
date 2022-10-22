5a5f484ca41920e99ffbf1941f3aaf77b6382978
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-22 11:43:21 +0100

    chore:mapping all the data to the dom

--- src/App.js
+++ src/App.js
@@ -13,6 +13,21 @@ function App() {
           <span>/</span> Slider
         </h2>
       </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, title, quote, name } = person;
+
+          return (
+            <article key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title} </p>
+              <p className="text">{quote} </p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+      </div>
     </section>
   );
 }
